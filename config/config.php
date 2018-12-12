ad94158c856dbeec877e8c97b758385686ab2c45
Jonatana Andriamasy <jonatana-aro-jesy.andriamasy@etu.univ-amu.fr>
2018-12-12 13:34:05 +0100

    base de données en PDO

--- config/config.php
+++ config/config.php
@@ -1,8 +1,4 @@
 <?php
-/*$link = mysqli_connect('mysql-videotheque.alwaysdata.net', '170657', 'Videotheque')
-or die('Pb de connexion au serveur: ' . mysqli_connect_error());
-mysqli_select_db($link, 'videotheque_db') or die ('Pb de sélection BD : ' . mysqli_error($link));*/
-
 $bdd = new PDO('mysql:host=mysql-videotheque.alwaysdata.net;dbname=videotheque_db', '170657', 'Videotheque');
 $bdd->exec('SET CHARACTER SET utf8');
 
